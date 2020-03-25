9aa86e5d680103deb4e26326a5a0cd3b4ddce0ae
Feniska <kat.nanivska@gmail.com>
2020-03-25 16:46:25 +0000

    .

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-
 const http = require('http');
-
+const url = require ('url')
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
+  query = url.parse (req.url,true).query;
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.end('Hello Kitty');
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}/?name=Kitty `);
 });
