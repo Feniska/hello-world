1b87ba49d9ec5180101874db600d0c6c1ac6ceac
Feniska <kat.nanivska@gmail.com>
2020-03-26 11:10:58 +0000

    .

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const http = require('http');
-const url = require ('url')
+
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  query = url.parse (req.url,true).query;
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Kitty');
-});
+const express = require("express")
+const app = express()
+const server = http.createServer(function(req,res) {
+
+app.get("/",(req,res) => res.send("Hello "))
+
+app.get("/?name=:id",function(req,res) {
+    res.send("Hello " + req.params.id);
+    
+})
+
+app.listen(3000)
+
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/?name=Kitty `);
+  console.log(`Server running at http://${hostname}:${port}/?name=`);
 });
